8fa3dd8da43c0df093cd6f50fd6bfb9fe3d77d71
Shashank Yadav <812shanky@gmail.com>
2023-05-30 22:22:02 +0530

    update

--- astrosphere/server/server.js
+++ astrosphere/server/server.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get("/api", (req, res) => {
-    res.json({"users": ["userone", "usertwo", "userthree"]})
+    res.json({"users": ["userone", "usertwo", "userthree", "userfour"]})
 });
 
 app.listen(5000, () => {
