5c5a237942768c44b842a5a97098b73a81c3e1a6
Shashank Yadav <812shanky@gmail.com>
2023-05-31 13:58:59 +0530

    update

--- astrosphere/client/src/components/App.jsx
+++ astrosphere/client/src/components/App.jsx
@@ -1,28 +1,40 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 function App() {
 
   const [backendData, setBackendData] = useState([]);
 
-  useEffect(() => {
-    fetch("/api").then(
-      response => response.json()
-    ).then(
-      data => setBackendData(data)
-    )
-  }, [])
+  // useEffect(() => {
+  //   fetch("/api").then(
+  //     response => response.json()
+  //   ).then(
+  //     data => setBackendData(data)
+  //   )
+  // }, []);
 
+  const shouldLog = useRef(true);
+  useEffect(() => {
+    if(shouldLog.current){
+      shouldLog.current = false;
+      fetch("/api").then(
+        response => response.json()
+      ).then(
+        data => setBackendData(data)
+      )
+    }
+  }, []);
+  
   return (
+
     <div>
-      
       {(typeof backendData.users === 'undefined') ? 
       (<p>Loading....</p>) : (
         backendData.users.map((user, i) => {
           return <p key={i}>{user}</p>
         })
       )}
-
     </div>
+
   )
 }
 
