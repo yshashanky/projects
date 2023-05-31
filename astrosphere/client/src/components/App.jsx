0a90bd109461f99f76d02d28e5b2aea881ae6881
Shashank Yadav <812shanky@gmail.com>
2023-05-31 13:45:53 +0530

    update

--- astrosphere/client/src/components/App.jsx
+++ astrosphere/client/src/components/App.jsx
@@ -2,15 +2,13 @@ import React, { useEffect, useState } from 'react'
 
 function App() {
 
-  const [backendData, setBackendData] = useState([{}]);
+  const [backendData, setBackendData] = useState([]);
 
   useEffect(() => {
     fetch("/api").then(
       response => response.json()
     ).then(
-      data => {
-        setBackendData(data)
-      }
+      data => setBackendData(data)
     )
   }, [])
 
