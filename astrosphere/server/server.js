c60ab01358e50998751c33bfac34cfd5e2901185
Shashank Yadav <812shanky@gmail.com>
2023-06-02 18:12:02 +0530

    update

--- astrosphere/server/server.js
+++ astrosphere/server/server.js
@@ -1,9 +1,24 @@
 const express = require('express')
+const axios = require('axios')
 
 const app = express()
 
 app.get("/api", (req, res) => {
     res.json({"users": ["userone", "usertwo", "userthree", "userfour"]})
+});
+
+app.get("/pictureOfDay", async(req, res) =>{
+    
+    try{
+
+        const response = await axios.get('https://api.nasa.gov/planetary/apod?api_key=SqDvmlkP2nm7fYyiJ1wSxIhWCicRU3dEod6qM7H3');
+        const responseData = response.data;
+        res.json(responseData);
+
+    } catch (error) {
+        console.error('Error making API call:', error.message);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
 });
 
 app.listen(5000, () => {
